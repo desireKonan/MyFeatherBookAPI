0b10c72059372a94626f79c4e40767a27ee28de2
desireKonan <harveykonan@gmail.com>
2025-07-11 15:00:55 +0000

    fix: Correction de la partir concernant Synthese

--- app/models/model.py
+++ app/models/model.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel
 from enum import Enum
 from dataclasses import dataclass
-
+from datetime import datetime
 
 class AttachmentType(Enum):
     """ Ici les types de documents utilises pour notre projet """
@@ -19,7 +19,16 @@ class Attachment(BaseModel):
 class Note(BaseModel):
     content: str
     attachments: list[Attachment]
-    created_at: str
-    updated_at: str
+    created_at: datetime
+    updated_at: datetime
+
+
+@dataclass
+class Synthesis(BaseModel):
+    id: int
+    url: str
+    is_generated: bool
+    created_at: datetime
+    updated_at: datetime
 
 
